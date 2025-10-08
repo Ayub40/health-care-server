e1534fec479f707c6bc9ddf23852d811248196d4
Ayub40 <ayubk4028@gmail.com>
2025-10-09 01:51:58 +0600

    57-5 Handling Image Upload using Multer

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,17 +1,17 @@
 import express, { NextFunction, Request, Response } from 'express'
 import { UserController } from './user.controller';
-// import { fileUploader } from '../../helper/fileUploader';
-// import { UserValidation } from './user.validation';
+import { fileUploader } from '../../helper/fileUploader';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
 router.post(
-    "/create-patient",UserController.createPatient
-    // fileUploader.upload.single('file'),
-    // (req: Request, res: Response, next: NextFunction) => {
-    //     req.body = UserValidation.createPatientValidationSchema.parse(JSON.parse(req.body.data))
-    //     return UserController.createPatient(req, res, next)
-    // }
+    "/create-patient",
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = UserValidation.createPatientValidationSchema.parse(JSON.parse(req.body.data))
+        return UserController.createPatient(req, res, next)
+    }
 
 )
 
