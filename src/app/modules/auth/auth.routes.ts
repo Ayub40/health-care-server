811c143c2dbcebfc0011c57d8c87070ed0d5f16f
Ayub40 <ayubk4028@gmail.com>
2025-10-10 00:51:09 +0600

    jwt Helper and auth service login fully complete

---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+
+const router = express.Router();
+
+router.post(
+    "/login",
+
+)
+
+export const authRoutes = router;
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { UserStatus } from "@prisma/client"
 import { prisma } from "../../shared/prisma"
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
+import { jwtHelper } from "../../helper/jwtHelper";
 
 const login = async (payload: { email: string, password: string }) => {
     const user = await prisma.user.findUniqueOrThrow({
@@ -15,9 +17,13 @@ const login = async (payload: { email: string, password: string }) => {
         throw new Error("Password is incorrect!")
     }
 
+    const accessToken = jwtHelper.generateToken({ email: user.email, role: user.role }, "abcd", "1h");
 
+    const refreshToken = jwtHelper.generateToken({ email: user.email, role: user.role }, "abcdefgh", "90d");
 
     return {
+        accessToken,
+        refreshToken,
         needPasswordChange: user.needPasswordChange
     }
 }
