fbc35b5f21564efbea9683077d96b83d6f1059f4
Ayub40 <ayubk4028@gmail.com>
2025-10-24 00:02:41 +0600

    get me, login, refresh-token, change-password, forgot-password, reset-password added

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,5 +13,24 @@ export default {
     api_secret: process.env.CLOUDINARY_API_SECRET,
   },
   openRouterApiKey: process.env.OPENROUTER_API_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+    refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    reset_pass_secret: process.env.RESET_PASS_TOKEN,
+    reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN,
+  },
+
+  salt_round: process.env.SALT_ROUNDS,
+  reset_pass_link: process.env.RESET_PASS_LINK,
+
+  emailSender: {
+    email: process.env.EMAIL_SENDER_USER,
+    app_pass: process.env.EMAIL_SENDER_PASS,
+  },
+
+
 };
