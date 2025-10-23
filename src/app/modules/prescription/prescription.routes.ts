1eb2d04c388258863b311616c588f09679df17b6
Ayub40 <ayubk4028@gmail.com>
2025-10-24 00:21:11 +0600

    patientPrescription added

--- src/app/modules/prescription/prescription.routes.ts
+++ src/app/modules/prescription/prescription.routes.ts
@@ -2,13 +2,31 @@ import { UserRole } from '@prisma/client';
 import express from 'express';
 import auth from '../../middlewares/auth';
 import { PrescriptionController } from './prescription.controller';
+// import { PatientController } from '../patient/patient.controller';
 const router = express.Router();
 
+
+router.get(
+    '/my-prescription',
+    auth(UserRole.PATIENT),
+    PrescriptionController.patientPrescription
+)
 
 router.post(
     "/",
     auth(UserRole.DOCTOR),
     PrescriptionController.createPrescription
 );
+
+// router.post(
+//     "/",
+//     auth(UserRole.DOCTOR),
+//     PrescriptionController.createPrescription
+// );
+
+// router.get(
+//     '/:id',
+//     PatientController.getByIdFromDB
+// );
 
 export const PrescriptionRoutes = router;
