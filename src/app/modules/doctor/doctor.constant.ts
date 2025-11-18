ac3a37e1d25370915fe959f67c78aabac531802b
Ayub40 <ayubk4028@gmail.com>
2025-11-19 03:14:06 +0600

    update some code

--- src/app/modules/doctor/doctor.constant.ts
+++ src/app/modules/doctor/doctor.constant.ts
@@ -1,4 +1,22 @@
-export const doctorFilterableFields = ["email", "contactNumber", "gender", "appointmentFee", "specialties", "searchTerm"]
+export const doctorSearchableFields: string[] = [
+    "name",
+    "email",
+    "contactNumber",
+    "address",
+    "qualification",
+    "designation",
+];
 
-// egolor data "string" e ase
-export const doctorSearchableFields = ["name", "email", "contactNumber"]
+export const doctorFilterableFields: string[] = [
+    "searchTerm",
+    "email",
+    "contactNumber",
+    "gender",
+    "specialties",
+];
+
+
+// export const doctorFilterableFields = ["email", "contactNumber", "gender", "appointmentFee", "specialties", "searchTerm"]
+
+// // egolor data "string" e ase
+// export const doctorSearchableFields = ["name", "email", "contactNumber"]
